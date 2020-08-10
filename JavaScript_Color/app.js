5039ab16898835313552cb910f517f029be83134
Talal Ahmed <56167664+talal02@users.noreply.github.com>
2020-08-11 00:57:33 +0500

    Add files via upload

---
+++ JavaScript_Color/app.js
@@ -0,0 +1,205 @@
+const simple = document.querySelector('#simple');
+const hex = document.querySelector('#hex');
+const body = document.querySelector('body');
+const colorInfo = document.querySelector('#color-info');
+const clickMe = document.querySelector('#click-me');
+const footer = document.querySelector('footer');
+
+body.style.backgroundColor = 'Yellow';
+
+const CSS_COLOR_NAMES = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+];
+
+const generateSimpleColor = () => {
+    return CSS_COLOR_NAMES[Math.floor( Math.random() * CSS_COLOR_NAMES.length )];
+}
+
+const generateHexColor = () => {
+    const hexValue = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
+    let hexCode = '#';
+    while( hexCode.length < 7 ){
+        hexCode += hexValue[ Math.floor( Math.random() * hexValue.length ) ];
+    }
+    return hexCode;
+}
+
+const callSimple = () => {
+    const color = generateSimpleColor();
+    colorInfo.innerHTML = `Background Color : ${color}`
+    body.style.backgroundColor = color;
+}
+
+const callHex = () => {
+    const color = generateHexColor();
+    colorInfo.innerHTML = `Background Color : ${color}`
+    body.style.backgroundColor = color;
+}
+
+simple.addEventListener('click', (e)=>{
+    footer.innerHTML = 'Simple';
+    callSimple();
+    e.preventDefault();
+});
+
+clickMe.addEventListener('click', (e)=>{
+    if(footer.innerHTML === 'Simple') {
+        callSimple();
+    } else {
+        callHex();
+    }
+    e.preventDefault();
+})
+
+hex.addEventListener('click', (e)=>{
+    footer.innerHTML = 'Hex';
+    callHex();
+    e.preventDefault();
+});
