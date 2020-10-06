f4de8e25b3fa1298cd82fa07524d3267ea73a458
Talal Ahmed <56167664+talal02@users.noreply.github.com>
2020-10-06 12:23:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const loading = () => {
 	document.querySelector('body').classList.remove('bg-info');
 	const loadingElements = document.querySelectorAll('.hide');
 	loadingElements[0].classList.add('d-none');
-	loadingElements[0].classList.remove('d-flex') const mainContent = document.querySelectorAll('.content') mainContent[0].classList.remove('d-none');
+	loadingElements[0].classList.remove('d-flex') 
+	const mainContent = document.querySelectorAll('.content') 
+	mainContent[0].classList.remove('d-none');
 	mainContent[1].classList.remove('d-none');
 	mainContent[2].classList.remove('d-none');
 	mainContent[3].classList.remove('d-none');
